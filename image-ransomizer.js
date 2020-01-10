7987e2e8800393cdeb579c55115d03d57c9665f5
Kerry Landis <59699040+kerrylandis@users.noreply.github.com>
2020-01-10 15:51:43 -0500

    Update image-ransomizer.js

--- image-ransomizer.js
+++ image-ransomizer.js
@@ -1,7 +1,7 @@
-var imagesArray = ["2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39"];
+var imagesArray = ["1","2","3","4","5"]; //you can set the array integer values dynamically
 var tempImagesArray = imagesArray.slice(0);
 
-getRandomImage = function(tempImagesArray,size){
+getRandomImage = function(tempImagesArray,size){ //call getRandomImage if no size is needed and remove the size input value
   if (tempImagesArray.length > 0){
     num = Math.floor(Math.random() * tempImagesArray.length);
     src = 'path/imagename'+ tempImagesArray[num] + '-' + size + '.jpg';
@@ -15,7 +15,7 @@ getRandomImage = function(tempImagesArray,size){
   return(src);
 }
 
-displayImage = function (){
+displayImage = function (){ //modify sizes or add new ones if you need to use this in several different locations
   var size = '220x220';
   getRandomImage(tempImagesArray,size);
 	return(src);
